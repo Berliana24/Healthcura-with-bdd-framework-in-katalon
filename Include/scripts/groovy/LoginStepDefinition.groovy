991f888f62bf222a39b186ff4f1f84b01c9664cb
Berliana24 <58837989+Berliana24@users.noreply.github.com>
2023-10-25 16:50:46 +0700

    Update Login Step Definition

--- Include/scripts/groovy/LoginStepDefinition.groovy
+++ Include/scripts/groovy/LoginStepDefinition.groovy
@@ -57,33 +57,34 @@ class LoginStepDefinition {
 	@When("User click button Make Appointment")
 	def User_click_button_Make_Appointment() {
 		WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/a_Make Appointment'))
-		
 	}
 
 	@Then("User will be redirect to Login Page")
 	def User_will_be_redirect_to_Login_Page() {
 		WebUI.verifyElementPresent(findTestObject("Object Repository/Page_CURA Healthcare Service/h2_Login"), 0)
 	}
-	
+
 	@When("User input (.*) and (.*)")
 	def User_input_username_and_password(String username, String password){
 		WebUI.setText(findTestObject("Object Repository/Page_CURA Healthcare Service/input_Username_username"), username)
 		WebUI.setText(findTestObject("Object Repository/Page_CURA Healthcare Service/input_Password_password"), password)
 	}
-	
+
 	@And("User click button Login")
 	def User_click_button_Login(){
 		WebUI.click(findTestObject("Object Repository/Page_CURA Healthcare Service/button_Login"))
 	}
-	
+
 	@Then("User will be redirect to page Make Appointment")
 	def User_will_be_redirect_to_page_Make_Appointment(){
 		WebUI.verifyElementPresent(findTestObject("Object Repository/Page_CURA Healthcare Service/h2_Make Appointment"), 0)
 	}
-	
+
 	@Then("Invalid username and password message will be display")
 	def Invalid_username_and_password_message_will_be_display(){
 		WebUI.verifyElementPresent(findTestObject("Object Repository/Page_CURA Healthcare Service/p_Login failed Please ensure the username and password are valid"), 0)
 		WebUI.closeBrowser()
 	}
+	
+
 }
