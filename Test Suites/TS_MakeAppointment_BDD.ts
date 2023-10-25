0922d5967ef73f750136f9d01a1dd1f858fa3ecf
Berliana24 <58837989+Berliana24@users.noreply.github.com>
2023-10-25 16:50:29 +0700

    Test Suite Make Appointment

---
+++ Test Suites/TS_MakeAppointment_BDD.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_MakeAppointment_BDD</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a455637f-cf92-48a1-9652-74d3f346cd56</testSuiteGuid>
+   <testCaseLink>
+      <guid>c08d5b25-d880-4fd6-ac40-0467018f8661</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_MakeAppointment_BDD</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
